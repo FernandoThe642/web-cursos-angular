3a66926cf2ebe282088d26cf308ff343044d9fe9
Fernando Sánchez Rodríguez <jsanchezr6@est.ups.edu.ec>
2024-11-21 09:43:21 -0500

    Estructura básica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { AgregarCursoComponent } from './pages/agregar-curso/agregar-curso.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'pages/home',
+        pathMatch: 'full'
+    },
+    {
+        path:'pages/home',
+        component: HomeComponent
+    },
+    {
+        path: 'pages/cursos',
+        component: CursosComponent
+    },
+    {
+        path: 'pages/agregar-curso',
+        component: AgregarCursoComponent
+    },
+    {
+        path: 'pages/contacto',
+        component: ContactoComponent
+    }
+    
+];
