d7c4b0779222046390b0bef048dd207e8c046d30
Fernando Sánchez Rodríguez <jsanchezr6@est.ups.edu.ec>
2024-11-21 11:27:50 -0500

    Funcionando

--- src/app/pages/agregar-curso/agregar-curso.component.ts
+++ src/app/pages/agregar-curso/agregar-curso.component.ts
@@ -1,12 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-curso',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './agregar-curso.component.html',
   styleUrl: './agregar-curso.component.scss'
 })
 export class AgregarCursoComponent {
+  nombreCurso: string = '';
+  nombreInstructor: string = '';
+  fechaInicio: string = '';
+  duracion: number | null = null;
+  descripcion: string = '';
+  mostrarErrores: boolean = false;
 
+  agregarCurso() {
+    if (this.validarFormulario()) {
+      const curso = {
+        id: Date.now(),
+        nombreCurso: this.nombreCurso,
+        nombreInstructor: this.nombreInstructor,
+        fechaInicio: this.fechaInicio,
+        duracion: this.duracion,
+        descripcion: this.descripcion
+      };
+
+      this.guardarCurso(curso)
+      this.reiniciarFormulario()
+      this.mostrarErrores = false
+    } else {
+      this.mostrarErrores = true
+    }
+  }
+
+  
+  validarFormulario(): boolean {
+    return this.nombreCurso !== '' && this.nombreInstructor !== '' && this.fechaInicio !== '' &&
+                                this.duracion !== null && this.descripcion !== '';
+  }
+
+
+  guardarCurso(curso: any) {
+    let cursos = JSON.parse(localStorage.getItem('cursos') || '[]');
+    cursos.push(curso);
+    localStorage.setItem('cursos', JSON.stringify(cursos));
+  }
+
+  // Reiniciar el formulario
+  reiniciarFormulario() {
+    this.nombreCurso = '';
+    this.nombreInstructor = '';
+    this.fechaInicio = '';
+    this.duracion = null;
+    this.descripcion = '';
+  }
 }
--- src/app/pages/cursos/cursos.component.ts
+++ src/app/pages/cursos/cursos.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
 export class CursosComponent {
+  
+  cursos: any[] = [];
+
+  ngOnInit() {
+    this.cargarCursos();
+  }
+
+  
+  cargarCursos() {
+    const cursosGuardados = JSON.parse(localStorage.getItem('cursos') || '[]');
+    this.cursos = cursosGuardados;
+  }
+
+  
+  eliminarCurso(id: number) {
+    this.cursos = this.cursos.filter(curso => curso.id !== id);
+    localStorage.setItem('cursos', JSON.stringify(this.cursos));
+  }
+
+  
+  mostrarDetalles(curso: any) {
+    curso.mostrarDetalles = !curso.mostrarDetalles;
+  }
 
 }
